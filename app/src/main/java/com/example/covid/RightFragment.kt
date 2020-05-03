79648ff1679dd96be0cdeea98695c62c231dd4af
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-05-03 10:28:42 +0530

    right fragment completed

--- app/src/main/java/com/example/covid/RightFragment.kt
+++ app/src/main/java/com/example/covid/RightFragment.kt
@@ -1,10 +1,17 @@
 package com.example.covid
 
+
+import android.content.Intent
+
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import androidx.fragment.app.Fragment
+
+import kotlinx.android.synthetic.main.fragment_right.*
 
 
 class RightFragment : Fragment() {
@@ -17,4 +24,22 @@ class RightFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_right, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        covidTv.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_VIEW)
+            callIntent.data = Uri.parse("tel:1075")
+            startActivity(callIntent)
+        }
+        ambulanceTv.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_VIEW)
+            callIntent.data = Uri.parse("tel:102")
+            startActivity(callIntent)
+        }
+        policeTv.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_VIEW)
+            callIntent.data = Uri.parse("tel:100")
+            startActivity(callIntent)
+        }
+    }
 }
